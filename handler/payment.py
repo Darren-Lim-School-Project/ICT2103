df2366047d19181c2e445428e6e8fa2f8c56f7be
kendricklq <65295251+kendricklq@users.noreply.github.com>
2021-11-30 17:48:10 +0800

    Completed check instock before checkout

--- handler/payment.py
+++ handler/payment.py
@@ -1,5 +1,7 @@
 import telegram
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+# Import of SQLite 3
+import sqlite3
 
 backToMainMenu = [
     [
@@ -15,3 +17,9 @@ def payment(update, context, totalAmount):
                             "Please make your payment of <b>$" + str('{:.2f}'.format(totalAmount)) + "</b>" + " to UEN Number: 1234567890" + "\n\n" +
                             "Thank you for your purchase!\n" +
                             "Use /start to start another transaction.", parse_mode="html", reply_markup=reply_markup)
+
+    # Setup connection to "ICT2103_Group32.db"
+    con = sqlite3.connect('ICT2103_Group32.db')
+    cur = con.cursor()
+
+    cur.execute("SELECT p.instock, cc.quantity, p.productName from Products P, Cart_Contents cc WHERE cc.productID = p.productID")
