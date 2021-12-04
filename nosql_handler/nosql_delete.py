5f482972785d8dfed3fcb0947e6a2bc8ec538703
Kendrick Lee <kendricklq>
2021-12-05 00:36:26 +0800

    Compeleted Cart and partial delete

--- nosql_handler/nosql_delete.py
+++ nosql_handler/nosql_delete.py
@@ -1,6 +1,6 @@
 import telegram
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-import sqlite3
+from firestoredb import db
 
 # Delete inline keyboard options
 nosqldeletekeyboard = [
@@ -18,93 +18,58 @@ def nosql_delete(update, context):
 
     query = update.callback_query
     reply_markup = InlineKeyboardMarkup(nosqldeletekeyboard)
+    counter = 0
     # Check given argument if correct syntax
     if (len(context.args) != 2):
         update.message.reply_text("Invalid commands!" + "\n" + "Syntax: /nosql_delete [Product ID] [Quantity]")
     else:
-        # Setup connection to "ICT2103_Group32.db"
-        con = sqlite3.connect('ICT2103_Group32.db')
-        cur = con.cursor()
-        # TODO Update customerID when ready
-        cur.execute("SELECT cartID from Shopping_Cart WHERE customerID=1 ORDER BY cartID DESC")
-        data = cur.fetchone()
-        if (data is not None):
-            # A shopping cart exists
-            # Check if the shopping cart is completed or abandoned
-            cur.execute("SELECT cartID FROM Completed_Cart WHERE cartID=" + str(data[0]))
-            ccac = cur.fetchall()
-            if (int(len(ccac)) > 0):
-                # A shopping cart was either completed or abandoned
-                # So technically cart is new and empty
-                update.message.reply_text("You have no items in your cart\, no products to remove" + "\n\n" +
-                            "Click on the 'Products' Button to browse products\!", parse_mode='MarkdownV2', reply_markup=reply_markup)
-            else:
-                # Shopping cart exist and not completed or abandoned
-                # Getting cart contents based on cartID
-                cur.execute("SELECT * FROM Cart_Contents WHERE cartID=" + str(data[0]))
-                cartItem = cur.fetchall()
-                print("cartItem ", cartItem)
-                print("Length cartItem ", len(cartItem))
-                #Check if shopping cart is empty
-                if (len(cartItem) == 0):
-                    # Shopping cart empty
-                    update.message.reply_text("You have no items in your cart\, no products to remove" + "\n\n" +
-                            "Click on the 'Products' Button to browse products\!", parse_mode='MarkdownV2', reply_markup=reply_markup)
-                else:
-                    # need check if argument productID is in cart
-                    counter = 0
-                    for i in cartItem:
-                        if str(context.args[0]) == str(i[1]):
-                            counter = counter + 1
-                        else:
-                            counter = counter
-                    # productID exist in cart
-                    if counter > 0:
-                        # Shopping cart not empty so continue to delete item
-                        cur.execute("SELECT quantity FROM Cart_Contents WHERE cartID=" + str(data[0]) + " AND productID=" + str(context.args[0]))
-                        totalQuantityInCart = cur.fetchone()
-                        totalQuantity = totalQuantityInCart[0]
-                        argumentQuantity = int(context.args[1])
-                        # If keyed in to remove 0
-                        if argumentQuantity == 0:
+        chatid = update.message.chat.id
+        docs = db.collection(u'Customers').document(str(chatid)).collection(u'Carts').stream()
+        for doc in docs:
+            toDict = doc.to_dict()
+            counter = counter + 1
+        # Check if got cart or not cart
+        # != 0 means got cart
+        print("toDict: ", toDict)
+        if counter != 0:
+            productList = []
+            # Check if cart got any product
+            anyProduct = toDict.get('Product')
+            print("anyProduct: ", anyProduct)
+            print("toDict.Products: ",  toDict["Product"][0]["Quantity"])
+
+            # If got product
+            if anyProduct is not None:
+                for a in anyProduct:
+                    if int(a.get('ProductID')) == int(context.args[0]):
+                        if int(context.args[1]) == 0:
                             update.message.reply_text("Unable to delete\, you requested to remove 0 quantity" + "\n\n" +
                                             "You may want to check the quantity", parse_mode='MarkdownV2', reply_markup=reply_markup)
-                        # Check if trying to remove negative quantity
-                        elif argumentQuantity < 0:
+                        elif int(context.args[1]) < 0:
                             update.message.reply_text("Unable to delete\, you requested to remove negative quantity" + "\n\n" +
                                             "You may want to check the quantity", parse_mode='MarkdownV2', reply_markup=reply_markup)
-                        # Check if trying to remove more than quantity in cart
-                        elif argumentQuantity > totalQuantity:
+                        elif int(a.get('Quantity')) < int(context.args[1]):
                             update.message.reply_text("You are trying to remove more quantity then what you have in cart" + "\n\n" +
                                             "You may want to check the quantity", parse_mode='MarkdownV2', reply_markup=reply_markup)
-                        # Check if trying to remove lesser then quantity in cart
-                        elif argumentQuantity < totalQuantity:
-                            leftover = totalQuantity - argumentQuantity
-                            print("leftover", leftover)
-                            print("cartID ", str(data[0]))
-                            print("productID ", str(context.args[0]))
-                            cur.execute("UPDATE Cart_Contents SET quantity=" + str(leftover) + " WHERE cartID=" + str(data[0]) + " AND productID=" + str(context.args[0]))
-                            cur.execute("SELECT productName FROM Products WHERE productID=" + str(context.args[0]))
-                            productNameData = cur.fetchone()
-                            productName = productNameData[0]
-                            con.commit()
-                            update.message.reply_text("Removed " + str(argumentQuantity) + " " + str(productName) + " from shopping cart", reply_markup=reply_markup)
-                            con.close()
-                        # Check if planning to remove all quantity
-                        elif argumentQuantity == totalQuantity:
-                            cur.execute("DELETE FROM Cart_Contents WHERE cartID=" + str(data[0]) + " AND productID=" + str(context.args[0]))
-                            cur.execute("SELECT productName FROM Products WHERE productID=" + str(context.args[0]))
-                            productNameData = cur.fetchone()
-                            productName = productNameData[0]
-                            con.commit()
-                            update.message.reply_text("Removed " + str(argumentQuantity) + " " + str(productName) + " from shopping cart", reply_markup=reply_markup)
-                            con.close()
-                    else:
-                        update.message.reply_text("You keyed in a product ID that does not exist in your cart" + "\n\n" +
-                                "You may want to check your command", parse_mode='MarkdownV2', reply_markup=reply_markup)
-        else:
-            # Shopping cart don't exist
-            # A shopping cart don't exist
-            # So technically cart is new and empty
-            update.message.reply_text("You have no items in your cart\, no products to remove" + "\n\n" +
-                        "Click on the 'Products' Button to browse products\!", parse_mode='MarkdownV2', reply_markup=reply_markup)
+                        elif int(a.get('Quantity')) == int(context.args[1]):
+                            print("Remove item from cart")
+                        elif int(a.get('Quantity')) > int(context.args[1]):
+                            doc_ref = db.collection(u'Customers').document(str(chatid)).collection(u'Carts').document(u'1')
+                            # for doc_refs in doc_ref:
+                            #     updateDict = doc_refs.to_dict()
+                            updatedInt = int(a.get('Quantity')) - int(context.args[1])
+                            for x in anyProduct:
+                                if int(x.get("ProductID")) == int(context.args[0]):
+                                    # x['Quantity'] == updatedInt 
+                                    field_updates = {"Quantity":updatedInt}
+                                    doc_ref.update(field_updates)
+
+            # If no product
+        #     else:
+        #         query.edit_message_text("*Cart:*" + "\n" +
+        #                         "You have no items in your cart\." + "\n" +
+        #                         "Click on the 'Products' Button to browse products\!", parse_mode='MarkdownV2', reply_markup=reply_markup)
+        # else:
+        #     query.edit_message_text("*Cart:*" + "\n" +
+        #                         "You have no items in your cart\." + "\n" +
+        #                         "Click on the 'Products' Button to browse products\!", parse_mode='MarkdownV2', reply_markup=reply_markup)
